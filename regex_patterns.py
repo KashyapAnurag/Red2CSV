94e3afb4a1b363f3af0871133519d92e15563b2b
Anurag  Kashyap <91251342+KashyapAnurag@users.noreply.github.com>
2025-07-04 17:48:33 +0530

    Update regex_patterns.py

--- regex_patterns.py
+++ regex_patterns.py
@@ -1,11 +1,10 @@
-# regex_patterns.py (hosted on GitHub)
+# regex_patterns.py
 import re
 
 def get_red_regex_patterns():
     return {
-        "re_pattern1": re.compile(r'^(\d+)\s+(\d{14})\s+(\d+)\s+(\d+)\s+'),
-        "re_pattern2": re.compile(r'^(\d{14})\s+(\d+)\s+(\d+)'),
-        "re_pattern3": re.compile(r'^(\d{2}/\d{2}/\d{2})\s+(\d{2}:\d{2})\s+(\d+)\s+(\d+)'),
-        "re_pattern4": re.compile(r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2})\s+(\d+)\s+(\d+)'),
-        "re_pattern5": re.compile(r'^(\d+)\s+(\d{2}/\d{2}/\d{2})\s+(\d{2}:\d{2})\s+(\d+)\s+(\d+)')
+        "re_api": re.compile(r'^(\d+)\s+(\d{14})\s+(\d+)\s+(\d+)'),
+        "re_radar": re.compile(r'^(\d{2}/\d{2}/\d{2})\s+(\d{2}:\d{2})\s+(\d+)\s+(\d+)'),
+        "re_alt1": re.compile(r'^(\d{14})\s+(\d+)\s+(\d+)'),  # optional
+        "re_alt2": re.compile(r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2})\s+(\d+)\s+(\d+)'),  # optional
     }
